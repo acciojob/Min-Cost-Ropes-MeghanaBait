208204e81dd00c0493e46737c2a009aa456e1314
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-15 21:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	// sort tc : nlogn
-	//normal key accessing: logn
-	
-let obj = {};
-	//storing values as keys and its frequency as values in objecrs
-	arr.forEach(item =>{
-		 obj[item] ? obj[item]++ : (obj[item] = 1)
-	});
+function mincost(arr) {
+    // Sorting the array in non-decreasing order
+    arr.sort((a, b) => a - b);
 
-	let totalCost = 0;
-	let i = 1, n = arr.length;
-	while(i <= n - 1){
-		let [a,b] = Object.keys(obj)[0];
-		if(obj[a] > 1){
-			//if first element has freq more than 1
-			let currCost=2 * a;
-			if(obj[a] === 2) delete obj[a];
-			else obj[a] -=2;
-			//adding current cost to obj
-			obj[currCost] ? obj[currCost]++ : obj[currCost]= 1;
-			totalCost += currCost;
-		}
-		i++;
-	}
+    let totalCost = 0;
 
-module.exports=mincost;
+    // Loop until there is only one rope left
+    while (arr.length > 1) {
+        // Take the two smallest ropes
+        const rope1 = arr.shift();
+        const rope2 = arr.shift();
+
+        // Connect the two ropes and add the cost to the total
+        const currentCost = rope1 + rope2;
+        totalCost += currentCost;
+
+        // Insert the connected rope back into the array in sorted order
+        let index = 0;
+        while (index < arr.length && arr[index] < currentCost) {
+            index++;
+        }
+        arr.splice(index, 0, currentCost);
+    }
+
+    return totalCost;
+}
+
+module.exports = mincost;
